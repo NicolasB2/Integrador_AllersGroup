91dc15e3a93f9d7f28e88fe9b7e1d104e57ceb92
Nicolas <Nicolas@BLUESKY>
2018-09-15 10:09:17 -0500

    Separate the program design in the following packages: model, Algorithms and view

---
+++ AllersGroup/Algorithm/Combination.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm
+{
+    static class Combination
+    {
+        public static void Merge<T>(List<T> original)
+        {
+            List<List<T>> input = new List<List<T>>();
+            original.ForEach(n => input.Add(new List<T> { n }));
+            Merge(input, original);
+        }
+
+        public static void Merge<T>(List<List<T>> input, List<T> original)
+        {
+            foreach (List<T> elements in input)
+            {
+                foreach (T data in original)
+                {
+                    if (!elements.Contains(data))
+                    {
+                        elements.Add(data);
+                    }
+                }
+            }
+        }
+
+    }
+}
